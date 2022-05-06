5302c399e2bc92a25e137c09ef4ddf9f2015f859
Glitch (runbf) <none>
2022-05-06 17:02:48 +0000

    v1.0 - added loops

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ if (location.protocol != "https:") {
 var input = document.getElementsByTagName("textarea")[0];
 var output = document.getElementsByTagName("p")[0];
 var code = "";
+var mobileBtnHandler = document.getElementById("mobileBtnHandler");
+
+const ua = navigator.userAgent;
+if (
+  /(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua) ||
+  /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+    ua
+  )
+) {
+  mobileBtnHandler.style.display = "block";
+} else {
+  mobileBtnHandler.style.display = "none";
+}
 
 function updateCode() {
   var inSplit = input.value.split("");
@@ -52,16 +65,20 @@ input.addEventListener("keydown", function (event) {
 document.addEventListener("keydown", function (event) {
   var code = event.keyCode || event.which;
   if (code === 69) {
-    if (input.style.display == "none") {
-      input.style.display = "block";
-    } else {
-      input.style.display = "none";
-    }
-  } else if (code === 83) {
+    toggleHide();
+  } else if (code === 82) {
     event.preventDefault();
     run();
   }
 });
+
+function toggleHide() {
+  if (input.style.visibility == "hidden") {
+    input.style.visibility = "visible";
+  } else {
+    input.style.visibility = "hidden";
+  }
+}
 
 function run() {
   var registers = [];
@@ -69,13 +86,22 @@ function run() {
   registers[14999] = 0;
   var codes = code.split("");
   var outputStr = "";
+  var inceptionA = 0;
+  var inceptionB = 1;
   for (let i = 0; i < codes.length; i++) {
+    if (codes[i] == "[") {
+      inceptionA++;
+      codes[i] += inceptionA.toString();
+    }
+  }
+  console.log(codes);
+  for (var i = 0; i < codes.length; i++) {
     if (
       codes[i] != "<" &&
       codes[i] != ">" &&
       codes[i] != "+" &&
       codes[i] != "-" &&
-      codes[i] != "[" &&
+      codes[i].split("")[0] != "[" &&
       codes[i] != "]" &&
       codes[i] != "," &&
       codes[i] != "."
@@ -108,7 +134,15 @@ function run() {
       registers[selectedRegister] = prompt(
         "Taking Input... (Will only take in first character)"
       ).charCodeAt(0);
-    } else {
+    } else if (codes[i] == "]") {
+      if (registers[selectedRegister] > 0) {
+        for (let x = 0; x < codes.length; x++) {
+          if (codes[x] == "[" + inceptionB.toString()) {
+            i = x;
+          }
+        }
+      }
+    } else if (codes[i].split("")[0] != "[") {
       alert(codes[i]);
     }
   }
